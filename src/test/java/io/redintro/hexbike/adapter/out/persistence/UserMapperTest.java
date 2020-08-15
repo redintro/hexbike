d7541e5f1d786401800359643380e8a439d4dce3
matt <matt@redintro.io>
2020-08-15 11:47:31 +0100

    Cleaned up import

--- src/test/java/io/redintro/hexbike/adapter/out/persistence/UserMapperTest.java
+++ src/test/java/io/redintro/hexbike/adapter/out/persistence/UserMapperTest.java
@@ -4,8 +4,9 @@ import io.redintro.hexbike.domain.User;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 class UserMapperTest {
     @Test
