1dd69310fa4eda04edfb93fc88184b91d0b660d4
matt <matt@redintro.io>
2020-09-03 10:25:51 +0100

    Updated to use environment agnostic settings

--- src/test-integration/java/io/redintro/hexbike/ContainerTestBase.java
+++ src/test-integration/java/io/redintro/hexbike/ContainerTestBase.java
@@ -4,24 +4,28 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-public class ContainerTestBase {
-    public static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
+/*
+ * A static initializer to create a shared database container for reuse across tests. Used in place of the JUnit 5
+ * @Testcontainers annotation to avoid having to set the testcontainers.reuse.enable=true property a local
+ * ~/.testcontainers.properties file.
+ */
+public abstract class ContainerTestBase {
+    static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
     static {
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:10")
                 .withDatabaseName("hexbike_it")
                 .withUsername("docker")
                 .withPassword("docker")
-                .withExposedPorts(5432)
                 .withReuse(true);
 
         POSTGRESQL_CONTAINER.start();
     }
 
     @DynamicPropertySource
-    public static void registerPgProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", () -> String.format("jdbc:postgresql://localhost:%d/hexbike_it", POSTGRESQL_CONTAINER.getFirstMappedPort()));
-        registry.add("spring.datasource.username", () -> "docker");
-        registry.add("spring.datasource.password", () -> "docker");
+    static void datasourceConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
     }
 }
