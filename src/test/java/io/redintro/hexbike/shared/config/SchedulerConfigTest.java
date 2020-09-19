84a69082f5c76a30444d670bf44b116160df376a
matt <matt@redintro.io>
2020-09-19 09:39:34 +0100

    Fixed failing test

--- src/test/java/io/redintro/hexbike/shared/config/SchedulerConfigTest.java
+++ src/test/java/io/redintro/hexbike/shared/config/SchedulerConfigTest.java
@@ -1,10 +1,14 @@
 package io.redintro.hexbike.shared.config;
 
 import net.javacrumbs.shedlock.core.LockProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import redis.embedded.RedisServer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -19,6 +23,22 @@ public class SchedulerConfigTest {
 
     @Autowired
     private ThreadPoolTaskScheduler taskScheduler;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer embeddedRedisServer;
+
+    @BeforeEach
+    public void setUp() {
+        embeddedRedisServer = new RedisServer(redisPort);
+        embeddedRedisServer.start();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        embeddedRedisServer.stop();
+    }
 
     @Test
     public void shouldInitialiseSchedulerConfig() {
