be1d08b46a283a57d47d070b5e821a452701f7e1
matt <matt@redintro.io>
2020-09-25 18:23:12 +0100

    Added package level javadoc

---
+++ src/main/java/io/redintro/hexbike/adapter/in/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains adapters that drive our application (because they call our application core). Adapters herein
+ * are categorised as inbound as the data flows in from the adapter to the application core.
+ */
+package io.redintro.hexbike.adapter.in;
---
+++ src/main/java/io/redintro/hexbike/adapter/out/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains adapters that are driven by our application (because they are called by our application core).
+ * Adapters herein are categorised as outbound as data flows out from the application core to the adapter.
+ */
+package io.redintro.hexbike.adapter.out;
