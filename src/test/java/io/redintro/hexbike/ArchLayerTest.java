7fff2bf578d5e3afbc13050eaae241422eef4a76
matt <matt@redintro.io>
2021-01-07 16:27:01 +0000

    Added ArchUnit and initial test

---
+++ src/test/java/io/redintro/hexbike/ArchLayerTest.java
@@ -0,0 +1,16 @@
+package io.redintro.hexbike;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+@AnalyzeClasses(packages = "io.redintro.hexbike")
+public class ArchLayerTest {
+    @ArchTest
+    public static final ArchRule shouldCheckLayerAccess =
+            layeredArchitecture()
+                    .layer("Shared").definedBy("..shared..")
+                    .whereLayer("Shared").mayNotBeAccessedByAnyLayer();
+}
